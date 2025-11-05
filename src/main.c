7e11af6f76ff86bba43f739c4d3ec8af1f4fc578
MitoKuribo <m.grebe98@gmail.com>
2025-11-05 23:48:59 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 
 // Temporary stubs for the modules that I havent written yet, note that these will be worked on later.
 void ui_show_title(void) { printf("\n=== The Beggars'Sect ==="); }
-int main_menu(void) { printf("\n[Menu] 1=Start, 2=Quit \n"); int c=getchar(); while(getchar()!= '/n'); return (c=='1') ? 1 : 0; }
+int main_menu(void) { printf("\n[Menu] 1=Start, 2=Quit \n"); int c=getchar(); while(getchar()!= '\n'); return (c=='1') ? 1 : 0; }
 void scene_run(void) { printf("\n[Scene] A short breeze passes...\n"); }
 void combat_run(void) { printf("\n[Combat] You swing your blade!\n"); }
 void ui_gameover(void) { printf("\n[Game Over]\n"); }
